1e701d56fd34df36a10a5ae993acf682e9d825be
Mark Debono <mdebono72@users.noreply.github.com>
2025-10-18 22:42:54 +0200

    Fix all instances of 'embeded' typo

--- .ci/windows_amd_base_files/run_amd_gpu_disable_smart_memory.bat
+++ .ci/windows_amd_base_files/run_amd_gpu_disable_smart_memory.bat
@@ -1,2 +1,2 @@
-.\python_embeded\python.exe -s ComfyUI\main.py --windows-standalone-build --disable-smart-memory
+.\python_embedded\python.exe -s ComfyUI\main.py --windows-standalone-build --disable-smart-memory
 pause
--- server.py
+++ server.py
@@ -586,7 +586,7 @@ class PromptServer():
                     "required_templates_version": required_templates_version,
                     "python_version": sys.version,
                     "pytorch_version": comfy.model_management.torch_version,
-                    "embedded_python": os.path.split(os.path.split(sys.executable)[0])[1] == "python_embeded",
+                    "embedded_python": os.path.split(os.path.split(sys.executable)[0])[1] == "python_embedded",
                     "argv": sys.argv
                 },
                 "devices": [
